149aafd1231b52eef42deb5d7b1e026e1c0db07c
Tanish Gupta <tanishgupta12389@gmail.com>
2024-12-31 16:38:29 +0530

    Ddding a digial Clock

--- digital clock/script.js
+++ digital clock/script.js
@@ -1,10 +1,7 @@
-function updateClock() {
+const updateClock = () => {
     const now = new Date();
-    const hours = String(now.getHours()).padStart(2, '0');
-    const minutes = String(now.getMinutes()).padStart(2, '0');
-    const seconds = String(now.getSeconds()).padStart(2, '0');
-    const timeString = `${hours}:${minutes}:${seconds}`;
+    const timeString = now.toLocaleTimeString("en", { hour12: true });
     document.getElementById('clock').textContent = timeString;
-}
+};
 setInterval(updateClock, 1000);
 updateClock(); 
