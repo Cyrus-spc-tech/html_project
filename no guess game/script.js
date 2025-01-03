10b8a656abef5f1bbdd6f6709c561c1b3e5a3cec
Tanish Gupta <tanishgupta12389@gmail.com>
2025-01-03 15:41:51 +0530

    No.  Gussing game adding

---
+++ no guess game/script.js
@@ -0,0 +1,21 @@
+let submit = document.getElementById("submit");
+let result = document.getElementById("result");
+let randomNum = Math.floor(Math.random() * 10) + 1;
+let attempt=0
+
+function guessGame() {
+    let guess = parseInt(document.getElementById("guess").value);
+    
+    if (guess === randomNum) {
+        result.innerHTML = "Congratulations! You guessed the correct number in " + attempt + " attempts!";
+    } else if (guess > randomNum) {
+        result.innerHTML = "You guessed too high!";
+        attempt++;
+    } else if (guess < randomNum) {
+        result.innerHTML = "You guessed too low!";
+        attempt++;
+    }
+
+}
+
+submit.addEventListener("click", guessGame);
